a288ee8264730725d7ae45390443b6442008cf05
Nidvar <djayshine87@hotmail.com>
2021-01-15 18:28:02 +1100

    finished

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log('pic in pic')
+const video = document.getElementById('video')
+
+const the_app = async ()=>{
+    try{
+        const x = await navigator.mediaDevices.getDisplayMedia();
+
+        video.srcObject = x;
+
+        video.onloadedmetadata = ()=>{
+            video.play()
+        }
+
+    } catch(error){
+        console.log(error)
+    }   
+}
+
+document.querySelector('button').addEventListener('click',async ()=>{
+    document.querySelector('button').disabled = true;
+
+    await video.requestPictureInPicture();
+
+    document.querySelector('button').disabled = false;
+})
+
+the_app();
